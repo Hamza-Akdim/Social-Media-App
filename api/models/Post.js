70ac8d4ee3a870837e7e50168e5f2e80b14a2613
ARGUIG <amine.arguig@enim.ac.ma>
2024-05-21 16:14:02 +0000

    nothing

--- api/models/Post.js
+++ api/models/Post.js
@@ -1,89 +1,89 @@
-    const mongoose = require("mongoose");
-    const Joi = require("joi");
-    const { User } = require("./user.js");
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const { User } = require("./user.js");
 
 
-    // Create Post Schema
-    const PostSchema = new mongoose.Schema({
-        userId : {
-            type : mongoose.Schema.Types.ObjectID,
-            ref : User,  // model reference
-            required : true,
-        },
-        description : {
-            type : String,
-            default : ""
-        },
-        postPicture: {
-            data: {
-                type : Buffer, // Binary data of the image
-            },
-            contentType: {
-                type : String,
-                default : "" ,
-            } // MIME type of the image (e.g., image/jpeg, image/png)
-        },
-        likes: {
-            type: Map,
-            of: Boolean,
-            default : {}
-        },
-        comments: {
-            type: Map,
-            of: new mongoose.Schema({
-                userId: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: 'User',
-                    required: true
-                },
-                text: {
-                    type: String,
-                    required: true
-                },
-                createdAt: {
-                    type: Date,
-                    default: Date.now
-                }
-            }),
-            default: {},
-            required : false,
-        }
+// Create Post Schema
+const PostSchema = new mongoose.Schema({
+    userId : {
+        type : mongoose.Schema.Types.ObjectID,
+        ref : User,  // model reference
+        required : true,
     },
-    {
-        timestamps : true,
-    })
-
-    // Create Post Model
-    const Post =  mongoose.model("Post",PostSchema);
-
-    // Validation Add Post
-    function addPostValidation(obj){
-        const schema = Joi.object({
-            userId : Joi.required(),
-            description : Joi.string(),
-            postPicture : Joi.object({
-                data: Joi.binary(),
-                contentType: Joi.string().default(""),
-            }).optional().default(),
-            likes: Joi.object().pattern(Joi.string(), Joi.boolean()).default({})
-        });
-        return schema.validate(obj);
+    description : {
+        type : String,
+        default : ""
+    },
+    postPicture: {
+        data: {
+            type : Buffer, // Binary data of the image
+        },
+        contentType: {
+            type : String,
+            default : "" ,
+        } // MIME type of the image (e.g., image/jpeg, image/png)
+    },
+    likes: {
+        type: Map,
+        of: Boolean,
+        default : {}
+    },
+    comments: {
+        type: Map,
+        of: new mongoose.Schema({
+            userId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+                required: true
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now
+            }
+        }),
+        default: {},
+        required : false,
     }
+},
+{
+    timestamps : true,
+})
 
-    // Validation Update Post
-    function updatePostValidation(obj){
-        const schema = Joi.object({
-            description : Joi.string(),
-            postPicture : Joi.object({
-                data: Joi.binary(),
-                contentType: Joi.string().default(""),
-            }).optional().default(),
-        });
-        return schema.validate(obj);
-    }
+// Create Post Model
+const Post =  mongoose.model("Post",PostSchema);
 
-    module.exports = {
-        addPostValidation,
-        updatePostValidation,
-        Post
-    }
+// Validation Add Post
+function addPostValidation(obj){
+    const schema = Joi.object({
+        userId : Joi.required(),
+        description : Joi.string(),
+        postPicture : Joi.object({
+            data: Joi.binary(),
+            contentType: Joi.string().default(""),
+        }).optional().default(),
+        likes: Joi.object().pattern(Joi.string(), Joi.boolean()).default({})
+    });
+    return schema.validate(obj);
+}
+
+// Validation Update Post
+function updatePostValidation(obj){
+    const schema = Joi.object({
+        description : Joi.string(),
+        postPicture : Joi.object({
+            data: Joi.binary(),
+            contentType: Joi.string().default(""),
+        }).optional().default(),
+    });
+    return schema.validate(obj);
+}
+
+module.exports = {
+    addPostValidation,
+    updatePostValidation,
+    Post
+}
