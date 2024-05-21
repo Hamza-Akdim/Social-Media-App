f65d3f664fb846a12e3056cf6495b1cc0e71ceea
ARGUIG <amine.arguig@enim.ac.ma>
2024-05-21 15:04:12 +0000

    add : Unauthorized Error handling

--- api/middlewares/verifyToken.js
+++ api/middlewares/verifyToken.js
@@ -9,7 +9,11 @@ exports.verifyToken = jwt({
 });
 
 // Verify Token & Authorize user
-exports.authorization = (req, res, next) => {
+exports.authorization = (err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    return res.status(401).json({ message: 'Invalid or missing token' });
+  }
+  next(err);
   if (req.params.id === req.auth.id || req.auth.isAdmin) {
     next();
   } else {
